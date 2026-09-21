1185e6f040787a71c51b2718de6aa1e63147cdac
cuiyu-2026 <1101047543@qq.com>
2026-09-21 17:32:31 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = “sb_publishable_zGpCLE3d-qOp5HO_0G9O2g_kTM9J52J”;
 
 export const supabase = supabaseUrl && supabaseAnonKey
   ? createClient(supabaseUrl, supabaseAnonKey, {
