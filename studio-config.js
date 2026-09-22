a5f8764cf35638eb893c34928ab1a014e7cab768
cuiyu-2026 <1101047543@qq.com>
2026-09-22 09:47:24 +0800

    Update Supabase publishable key in config

--- studio-config.js
+++ studio-config.js
@@ -1,4 +1,4 @@
 window.STUDIOVERSE_CONFIG = {
   url: "https://ldbucqyygsdgmsrytimm.supabase.co",
-  key: "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY"
+  key: "sb_publishable_zGpCLE3d-qOp5HO_0G9O2g_kTM9J52J"
 };
